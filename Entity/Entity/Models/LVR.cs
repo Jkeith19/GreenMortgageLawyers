060977a9e136adc4ce21a5d4befd8aadcb86f3a4
wEggoy20xx@gmail.com <wEggoy20xx@gmail.com>
2025-01-21 23:33:07 +0800

    Create class library project for entity

---
+++ Entity/Entity/Models/LVR.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity.Entity.Models
+{
+    public class LVR
+    {
+        public int Id { get; set; }
+        public decimal LoanAmount { get; set; }
+        public decimal PropertyValue { get; set; }
+        public decimal LoanValuationRatio { get; set; }
+    }
+}
